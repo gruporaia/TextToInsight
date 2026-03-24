20b0cdbd6910c1a8e4481498011e6c47ca39572d
calixto <joaopedro.calixto10@gmail.com>
2026-03-24 17:20:39 -0300

    gemini-2.0-flash (indisponível atualmente -> gemini-2.5-flash. Todos os testes executados com sucesso

--- src/nodes/planner.py
+++ src/nodes/planner.py
@@ -15,7 +15,7 @@ from ..state import EstadoTextToInsight
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
 )
 
